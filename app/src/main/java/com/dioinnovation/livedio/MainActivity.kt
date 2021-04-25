02dac6dfab2b21a17a021ceac59ce1562f807a64
mayconfuzita <mayconfuzita@gmail.com>
2021-04-25 17:09:32 -0300

    first commit

---
+++ app/src/main/java/com/dioinnovation/livedio/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.dioinnovation.livedio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvList: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        iniciaViews()
+    }
+
+    private fun iniciaViews(){
+        rvList = findViewById(R.id.rv_list)
+    }
+}
